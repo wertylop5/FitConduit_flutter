b2acbc2b6b4dc6cba393246a53c81d913159b552
wertylop5 <slin55055@gmail.com>
2018-07-17 12:19:49 -0400

    Sort of replicated the original UI

--- lib/main.dart
+++ lib/main.dart
@@ -42,36 +42,59 @@ class _HomeWidgetState extends State<HomeWidget> {
         ],
       ),
       body: Container(
+        padding: EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text("Cables"),
-            ListView(
-              shrinkWrap: true,
+            Text(
+              "Cables",
+              textScaleFactor: 1.5,
+            ),
+            Container(
+              height: 300.0,
+              child: ListView(),
             ),
             Row(
               children: <Widget>[
-                RaisedButton(
+                FlatButton(
                   onPressed: () {},
-                  child: Text("Button"),
+                  child: Text("ADD A NEW CABLE"),
                 ),
                 FlatButton(
                   onPressed: () {},
-                  child: Text("BUTTON"),
+                  child: Text("ADD A NEW UNNAMED CABLE"),
                 ),
               ],
             ),
             Row(
               children: <Widget>[
-                Text("Select conduit type"),
-                Text("Select conduit type"),
+                Container(
+                  padding: EdgeInsets.only(
+                    right: 15.0,
+                  ),
+                  child: Text("Select conduit type"),
+                ),
+                DropdownButton<String>(
+                  onChanged: (String value) {},
+                  items: <DropdownMenuItem<String>>[
+                    DropdownMenuItem<String>(
+                      child: Text("Conduit 1"),
+                    ),
+                    DropdownMenuItem<String>(
+                      child: Text("Conduit 2"),
+                    ),
+                  ],
+                ),
               ],
             ),
             RaisedButton(
               onPressed: () {},
               child: Text("Calculate"),
             ),
-            Text("Results"),
+            Text(
+              "Results",
+              textScaleFactor: 1.5,
+            ),
           ],
         ),
       ),
