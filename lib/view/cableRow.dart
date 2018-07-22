cfca5166d507921336d084272eb09a1f8a3293aa
wertylop5 <slin55055@gmail.com>
2018-07-22 15:14:35 -0400

    styling the listview rows

---
+++ lib/view/cableRow.dart
@@ -0,0 +1,51 @@
+import "package:flutter/material.dart";
+
+/*
+  A widget used to represent a cable in the listview
+*/
+
+class CableRow extends StatefulWidget {
+  @override
+  State createState() => _CableRowState();
+}
+
+class _CableRowState extends State<CableRow> {
+  int _cableNum;
+  String _name;
+  //Cable _cable;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 10.0,
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              right: 15.0,
+            ),
+            child: Text(
+              "Cable 10",
+              textScaleFactor: 1.4,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: Text(
+                "Cable name",
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              "Amount: 10",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
